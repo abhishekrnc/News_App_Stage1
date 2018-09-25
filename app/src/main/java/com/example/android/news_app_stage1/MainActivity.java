fcf7ca43992462a2da2f01fc0c6a95a76bc51ddd
Abhishek <abhishektupudana@gmail.com>
2018-09-26 02:34:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/news_app_stage1/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.android.news_app_stage1;
+
+
+import android.app.LoaderManager;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Context;
+import android.content.Intent;
+import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements LoaderCallbacks<List<News>> {
+    private NewsAdapter mAdapter;
+    private TextView mNoContentTextView;
+    private final String BASE_URL = "https://content.guardianapis.com/search?&show-tags=contributor&api-key=";
+    private final String API_KEY = "0de58a9d-2c93-44c1-890f-884f9915a5f3";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView newsListView = findViewById(R.id.list_item);
+        mNoContentTextView = findViewById(R.id.nocontent_text_view);
+        newsListView.setEmptyView(mNoContentTextView);
+        mAdapter = new NewsAdapter(this, new ArrayList<News>());
+
+        newsListView.setAdapter(mAdapter);
+        newsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                News currentNews = mAdapter.getItem(position);
+                Uri newsUri = Uri.parse(currentNews.getmUrl());
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, newsUri);
+                startActivity(websiteIntent);
+            }
+        });
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            LoaderManager loaderManager = getLoaderManager();
+            loaderManager.initLoader(0, null, this);
+        } else {
+            View loadingIndicator = findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.GONE);
+            mNoContentTextView.setText(R.string.interneterrormessages);
+        }
+    }
+
+    @Override
+    public Loader<List<News>> onCreateLoader(int id, Bundle args) {
+        return new NewsLoader(this);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<News>> loader, List<News> news) {
+        View loadingIndicator = findViewById(R.id.loading_indicator);
+        loadingIndicator.setVisibility(View.GONE);
+        mNoContentTextView.setText(R.string.contneterrormessages);
+        mAdapter.clear();
+
+        if (news != null && !news.isEmpty()) {
+            mAdapter.addAll(news);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<News>> loader) {
+        mAdapter.clear();
+    }
+}
